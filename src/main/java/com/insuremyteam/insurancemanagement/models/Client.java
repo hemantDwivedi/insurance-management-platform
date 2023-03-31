6261a47ec80faa12906a3ad44848933959007872
hemantDwivedi <hemantdwiwedi@gmail.com>
2023-03-31 13:05:03 +0530

    Initializing insurance project

---
+++ src/main/java/com/insuremyteam/insurancemanagement/models/Client.java
@@ -0,0 +1,11 @@
+package com.insuremyteam.insurancemanagement.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "client")
+public class Client {
+
+
+}
